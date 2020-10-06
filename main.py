2b9019c20147c064963ab1062e2b50f31c8d5c60
a5892731 <71212857+a5892731@users.noreply.github.com>
2020-10-06 16:43:33 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+
+file = open('log.txt', 'r')
+
+for line in file:
+    print(line, end='')
+
+
+file.close()
