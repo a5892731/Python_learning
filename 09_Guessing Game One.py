908a284e2e558b91cd125363cec2ddd46d47ac25
a5892731 <a5892731@o2.pl>
2021-01-03 14:40:31 +0100

    lesson 9

--- 09_Guessing Game One.py
+++ 09_Guessing Game One.py
@@ -10,3 +10,43 @@ Extras:
 
 '''
 
+from random import randint
+
+
+# >>>>>>>>>>>>>>>>>>>>>>> MAIN <<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+while True:
+
+    guesses = 1
+    random_number = randint(1, 9)
+
+    while True:
+
+
+
+        print("guess the number from 1 to 9")
+        try:
+            user_guess = int(input(">>> "))
+        except ValueError:
+            print("wrong data")
+        if user_guess < 1 or user_guess > 9:
+            print("data out of range")
+        elif user_guess < random_number:
+            print("wrong gues, too low")
+        elif user_guess > random_number:
+            print("wrong gues, too high")
+        elif user_guess == random_number:
+            print("NICE! Exactly right")
+            print("You finished at {} attempt".format(guesses))
+            break
+
+        guesses += 1
+
+    next_game_choice = input("\n>>> do you want to continue Y/N?: ")
+    if next_game_choice == "y" or next_game_choice == "Y":
+        continue
+    else:
+        print("exit")
+        break
