d5652afcb77ada74806e8f60c850b145a4ff62dd
a5892731 <a5892731@o2.pl>
2021-01-03 11:51:48 +0100

    lesson 6

--- 06_String Lists.py
+++ 06_String Lists.py
@@ -2,3 +2,15 @@
 Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 '''
 
+def palindrome_of_string_check(string):
+    print("inputed string: " + string_from_user + "; string writen backwards: " + string_from_user[::-1])
+    if string_from_user == string_from_user[::-1]:
+        print("this string is palindrome")
+    else:
+        print("this string is not palindrome!")
+
+
+print("program that check string is a palindrome or not")
+string_from_user = input("give me a string: ")
+
+palindrome_of_string_check(string_from_user)
