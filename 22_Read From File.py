918c00bf2683bc3cc86e739431f6dcc3dd77d426
jakubz <a5892731@o2.pl>
2021-01-05 14:57:37 +0100

    lesson 22 update

--- 22_Read From File.py
+++ 22_Read From File.py
@@ -18,14 +18,14 @@ def get_names_from_file(gender = "Male"):
 
     def choose_gender_of_name(file_bufor, gender):
 
-        GENDER_DICTIONARY = {"Male": 1, "Female": 2}
+        GENDER_DICTIONARY = {"Rank": 0, "Male": 1, "Female": 2}
 
         names_bufor = []
         for line in range(1, len(file_bufor)):
             names_bufor.append(file_bufor[line][GENDER_DICTIONARY[gender]])
         return names_bufor
 
-    file = open('22_Read From File.txt', 'r', encoding="utf-8")
+    file = open('22_Read From File.txt', 'r', encoding="utf-8") # most popular names ranking in file
     file_bufor = []
 
     for line in file:
