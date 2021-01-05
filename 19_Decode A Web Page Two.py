0e02be9f71a796fc8bf200cf9891be9b6c38e62e
jakubz <a5892731@o2.pl>
2021-01-05 11:49:05 +0100

    lesson 18 undone

--- 19_Decode A Web Page Two.py
+++ 19_Decode A Web Page Two.py
@@ -12,3 +12,20 @@ This will just print the full text of the article to the screen. It will not mak
 we will learn how to write this text to a .txt file.
 '''
 
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# >>>>>>>>>>>>>>>>>>> MAIN <<<<<<<<<<<<<<<<<<<<<
+
+
+
+url = requests.get('https://web.archive.org/web/20140529103427/http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture')
+html = url.text
+page = BeautifulSoup(html, 'html.parser')
+match = page.find_all('div', 'parbase cn_text')
+page_list = [[k.get_text() for k in i.find_all('p')] for i in match]
+for i in page_list[:-2]:
+    for k in i:
+        print(k + '\n')
