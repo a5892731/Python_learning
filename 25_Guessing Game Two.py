47c4e80d6e37c1b902afb66398ca885cff28be76
a5892731 <a5892731@o2.pl>
2021-01-06 18:11:17 +0100

    lesson 25 in progress

--- 25_Guessing Game Two.py
+++ 25_Guessing Game Two.py
@@ -13,3 +13,27 @@ right in the middle of the range), and then increase / decrease by 1 as needed. 
 try to find the optimal strategy! (We’ll talk about what is the optimal one next week with the solution.)
 '''
 
+def drow_game_bord(height = 3, width = 3, field_height = 3, field_width = 3):
+
+    #y_pole is actual pole number in y axix
+    #x_pole is actual pole number in x axis
+
+    for y_pole in range(height):
+        for row in range(field_height):
+            for x_pole in range(width + 1):  # extra "1" is for generating end in last pole
+                if row == 0 or row == (field_height - 1): # up and down elements of pole
+                    if x_pole != width:
+                        print(" " + field_width * "-", end= "")
+                    elif x_pole == width:
+                        print(" ", end= "")
+                if row > 0 and row < (field_height - 1): # middle elements of pole
+                    if x_pole != width:
+                        print("|" + field_width * " ", end= "")
+                    elif x_pole == width:
+                        print("|", end= "")
+            print() # end of line "\n"
+
+# >>>>>>>>>>>>>>>>>>> MAIN <<<<<<<<<<<<<<<<<<<<<
+
+
+drow_game_bord()
