35f456a970a17edb4b35cda69d430a6edffcdd81
jakubz <a5892731@o2.pl>
2021-01-08 15:09:31 +0100

    lesson 30

--- 30_Pick Word.py
+++ 30_Pick Word.py
@@ -9,3 +9,25 @@ Each line in the file contains a single word.
 https://en.wikipedia.org/wiki/Peter_Norvig
 http://norvig.com/ngrams/sowpods.txt
 '''
+
+from linecache import getline
+from random import randint
+
+def file_lines_count(file_address):
+    file = open(file_address, 'r')
+    lines_count = 0
+    for line in file:
+        lines_count += 1
+    file.close()
+    return lines_count
+
+
+
+# >>>>>>>>>>>>>>>>>>>> main <<<<<<<<<<<<<<<<<<<<<<<<
+file_address = "30_Pick Word - sowpods.txt"
+length_of_file = file_lines_count(file_address)
+random_line_number = randint(1, length_of_file)
+
+print("\nfile: " + file_address + "\n")
+print("Random word: " + str(getline(file_address, random_line_number)) +
+      "from row " + str(random_line_number) + " of " +str(length_of_file))
