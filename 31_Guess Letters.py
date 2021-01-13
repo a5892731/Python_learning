cdd31a6e30acb8d2b50d12d94217a9af0f8f8500
jakubz <a5892731@o2.pl>
2021-01-13 12:07:10 +0100

    HANG MAN

--- 31_Guess Letters.py
+++ 31_Guess Letters.py
@@ -23,5 +23,92 @@ Incorrect!
 E _ _ _ _ _ _ _ E
 ...
 And so on, until the player gets the word.
+
+       |||||||||||||||
+       |||  ||      |
+       |||||      ('_')
+       |||        \ | /
+       |||         ( )
+       |||         / \
+|||||||||||||||||
+
+
 '''
 
+from Lesson31_functions import draw_hangman
+
+from linecache import getline
+from random import randint
+
+def file_lines_count(file_address):
+    file = open(file_address, 'r')
+    lines_count = 0
+    for line in file:
+        lines_count += 1
+    file.close()
+    return lines_count
+
+def random_word_generator(file_address):
+    length_of_file = file_lines_count(file_address)
+    random_line_number = randint(1, length_of_file)
+    random_word = getline(file_address, random_line_number)
+    return random_word
+
+def blanc_guessed_word_generator(correct_word_len):
+    output = ""
+    for letter in range(correct_word_len):
+        output = output + "_"
+    return output
+
+def guessed_letter_check(error_count, guessed_letter, guessed_word, correct_word):
+
+    new_guessed_word = ""
+
+    if guessed_letter in correct_word:
+        for letter_num in range(len(correct_word)):
+            if guessed_letter == correct_word[letter_num]:
+                new_guessed_word += guessed_letter
+            else:
+                new_guessed_word += guessed_word[letter_num]
+
+        return new_guessed_word, error_count
+    else:
+        error_count += 1
+        return guessed_word, error_count
+
+# >>>>>>>>>>>>>>>>>>>> main <<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+error_count = 0
+
+guessed_letter = ""
+#correct_word = random_word_generator("30_Pick Word - sowpods.txt")  # English words list
+correct_word = random_word_generator("31_Pick Word - SLOWA PL.txt")  # Polish word list
+correct_word = correct_word.rstrip("\n").upper()
+guessed_word = blanc_guessed_word_generator(len(correct_word))
+
+
+while True:
+
+    draw_hangman(error_count)
+    #print("\nCorrect word: " + correct_word )  # FOR APP TEST
+    print("Your word:    " + guessed_word )
+    print("Number of errors: " + str(error_count) )
+
+    guessed_letter = input("Give me a letter: ")
+    guessed_word, error_count = guessed_letter_check(error_count, guessed_letter.upper(), guessed_word, correct_word)
+
+
+
+
+    if error_count >= 6:
+        draw_hangman(error_count)
+        print("YOU WERE HANGED!")
+        print("Your word:    " + correct_word)
+        break
+
+    if "_" not in guessed_word:
+        draw_hangman(error_count)
+        print("YOU ARE ALIVE!")
+        break
